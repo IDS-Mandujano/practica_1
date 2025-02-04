af42bc834a3f9b76c5beab6cd7fdd601d0e01377
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2025-02-04 12:01:07 -0600

    Add server 1 - 04/02/25

--- server1/main.go
+++ server1/main.go
@@ -1,1 +1,15 @@
 package server1
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/users", getUsers)
+	r.POST("/users", createUser)
+	r.GET("/longpoll", longPolling)
+
+	r.Run(":8080")
+}
